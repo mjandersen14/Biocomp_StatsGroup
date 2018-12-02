a940c677dda04998b9086f9efcca68d42a01a545
Marissa Andersen <marissaandersen@marissas-air.dhcp.nd.edu>
2018-12-02 12:00:38 -0500

    got a p-val

--- ProjectQ2.py
+++ ProjectQ2.py
@@ -33,32 +33,32 @@ fit=minimize(regression,initialGuess,method="Nelder-Mead",options={'disp': True}
 
 def null(p,obs):
     B0=p[0]
-    B1=p[1]
-    sigma=p[2]
+    sigma=p[1]
     expected=B0
     nll=-1*norm(expected,sigma).logpdf(obs.growth).sum()
     return nll 
 
-initialGuess=numpy.array([1,1,1])
+initialGuess=numpy.array([1,1])
 fitnull=minimize(null,initialGuess,method="Nelder-Mead",options={'disp': True},args=sugar)
 
+print (fit,fitnull)
 # fit is a variable that contains an OptimizeResult object
 # attribute 'x' is a list of the most likely parameter values
-print(fit)
+
 
 from scipy import stats 
 
 teststat=2*(fitnull.fun-fit.fun)
-df=len(fit.x)-len(fitnull.x)
-pval=1-stats.chi2.cdf(teststat,df) #this didn't work 
+df=len(fit.x)-len(fitnull.x) #can't be 0 so what is wrong? 
+pval=1-stats.chi2.cdf(teststat,df)  
 print (pval)
 
 
 
 
 #find a p-value 
-#sentence about plot 
-#do we care about the effect of sugar on growth 
+#sentence about plot do we need a regression line on our plot? 
+#do we care about the effect of sugar on growth ... answer 
 
 
 
