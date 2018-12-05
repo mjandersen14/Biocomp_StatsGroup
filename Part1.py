2d9cf70984f3d556f7700a6218bb88110239c9c6
Marissa Andersen <marissaandersen@marissas-air.dhcp.nd.edu>
2018-12-05 18:45:26 -0500

    cleaned up

--- Part1.py
+++ Part1.py
@@ -15,37 +15,22 @@ from plotnine import *
 from scipy.optimize import minimize
 from scipy.stats import norm
 from scipy import stats
-#importing data
 
+#importing data
 anti=pd.read_csv("antibiotics.csv", header=0, sep=",")
 
 #visualizing data
-
 ggplot(anti, aes(x="trt",y="growth"))+geom_boxplot()
 
-#running ANOVA
-N=16
-y=.4*x+5
-# add some "noise" to y and put the variables in a dataframe
-y=y+np.random.randn(N)
+#dummy-code for data 
 antifixNull=pd.DataFrame({"x":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                           "y": anti.growth})
 antifixA4=pd.DataFrame({"x1":[0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0],
                       "x2":[0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0], 
                       "x3":[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
                       "y": anti.growth})
-A2=pd.DataFrame({"x1":[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1]})
-A4=pd.DataFrame({"x1":[0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
-                      "x2":[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0], 
-                      "x3":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1]})
-A8=pd.DataFrame({"x1":[0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                      "x2":[0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-                      "x3":[0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
-                      "x4":[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
-                      "x5":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0],
-                      "x6":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0],
-                      "x7":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1]})
 
+#define null and ANOVA functions 
 def NullMod(p,obs):
     B0=p[0]
     sigma=p[1]
@@ -63,24 +48,23 @@ def ANOVA(p,obs):
     nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
     return nll
 
-### estimate parameters by minimizing the negative log likelihood
+#estimate parameters by minimizing the negative log likelihood
+#fit is a variable that contains an OptimizeResult object
+#attribute 'x' is a list of the most likely parameter values
 initialGuess=np.array([1,1])
 initialGuess1=np.array([1,1,1,1,1])
 fitNull=minimize(NullMod,initialGuess,method="Nelder-Mead",options={'disp': True},args=antifixNull)
 fitAnova=minimize(ANOVA,initialGuess1,method="Nelder-Mead",options={'disp': True},args=antifixA4)
-# fit is a variable that contains an OptimizeResult object
-# attribute 'x' is a list of the most likely parameter values
+
 print(fitAnova.x)
 
-
+#compare the null and 2-way ANOVA to get a p-value 
 teststat=2*(fitNull.fun-fitAnova.fun)
 df2=len(fitAnova.x)-len(fitNull.x)
 p=1-stats.chi2.cdf(teststat,df2)
 
-print (teststat)
-print (df2)
 print (p)
 
-
+#sentence on p-value and if based off of that we are able to reject the null or not and thus if we caer about sugar on growth 
 
 
